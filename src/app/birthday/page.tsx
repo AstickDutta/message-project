0e82abf5c6427c82fc835557fe704a1bcec1c9ca
rittick dutta <astickdutta19@gmail.com>
2025-03-24 12:33:53 +0530

    initial commit

--- src/app/birthday/page.tsx
+++ src/app/birthday/page.tsx
@@ -1,5 +1,22 @@
-// src/app/birthday/page.tsx
+// // src/app/birthday/page.tsx
 import BirthdayCard from '@/components/BirthdayCard';
+
+// interface BirthdayPageProps {
+//   searchParams: {
+//     name: string;
+//   };
+// }
+
+// export default function BirthdayPage({ searchParams }: BirthdayPageProps) {
+//   const name = searchParams.name || 'Guest';
+
+//   return (
+//     <div className="min-h-screen bg-gradient-to-r from-pink-200 to-purple-200">
+//       <BirthdayCard name={name} />
+//     </div>
+//   );
+// }
+
 
 interface BirthdayPageProps {
   searchParams: {
